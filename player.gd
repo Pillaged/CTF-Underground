57594604bd74f698eaee5d4da0320eec48182471
Mason Kohls <mason.kohls1999@gmail.com>
2022-04-14 16:25:55 -0700

    Adds collision arrows to player.

--- player.gd
+++ player.gd
@@ -20,6 +20,13 @@ var directions = {
 	"move_down":  Vector2.DOWN,
 }
 
+onready var rays = {
+	"move_left": $ray_move_left,
+	"move_right":  $ray_move_right,
+	"move_up":  $ray_move_up,
+	"move_down":  $ray_move_down,
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	position = position.snapped(Vector2.ONE * tile_size)
@@ -28,25 +35,37 @@ func action_sort(x, y):
 	return actions[x] > actions[y]
 
 func _process(delta):
-	if not tween.is_active():
-		var dir = get_new_direction()
-		if dir.length() > 0 :
-			var new_pos = dir * tile_size + position
-			tween.interpolate_property(self, "position", position, position + dir * tile_size, 1.0/speed, Tween.TRANS_LINEAR, 0)
-			tween.start()
-	
+	try_move()
+
 	$AnimatedSprite.play()
+
+func try_move():
+	# Currently moving
+	if tween.is_active():
+		return
+	
+	# Get input direction
+	var dir = get_new_direction()
+	if dir == null:
+		return
+	
+	# Check if new position has a wall
+	if rays[dir].is_colliding():
+		return
+	
+	# Move to new tile
+	var new_pos = directions[dir] * tile_size + position
+	tween.interpolate_property(self, "position", position, new_pos, 1.0/speed, Tween.TRANS_LINEAR, 0)
+	tween.start()
 
 func get_new_direction(): 
 	var inputs = actions.keys()
 	inputs.sort_custom(self, "action_sort")
 	
-	var new_direction = Vector2.ZERO
 	for input in inputs:
 		if Input.is_action_pressed(input):
-			new_direction = directions[input]
-			break
-	return new_direction
+			return input
+	return null
 
 func _unhandled_input(event):
 	for dir in actions.keys():
